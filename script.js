be875e12138dd69bd30face716fb666787fc0977
rikilega <rosedoland33@yahoo.com>
2023-03-18 17:25:21 -0700

    able to fix errors parenthises and spelling and order issues working on loop if password length input is not valid.

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ function writePassword() {
   var passwordLength = prompt("Enter your password length. It must have at least 8 and no more than 128 charcters");
   
   //validate password length
-  if (passwordLength < 8 || passwordLength>128 || !Number.isIntegar(passwordLength)) {
+  if (passwordLength >= 8 && 128 >= passwordLength && Number.isInteger(parseInt(passwordLength))) {
+  confirm(`Your password length is set to ${passwordLength}.`)
+  } else if(passwordLength < 8 || passwordLength>128 || !Number.isIntegar(passwordLength)) {
   prompt("Please enter a valid password length. You must use numerical characters and set the length from 8 to 128.");
-  } else if (passwordLength >= 8 && 128 >= passwordLength && Number.isInteger(parseInt(passwordLength))) {
-    confirm("Your password length is set to " + passwordLength + ".")
   }
   }
   
