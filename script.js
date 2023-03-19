189089f3aeccb0890cc7417e5b9e49b2b6513ccd
rikilega <rosedoland33@yahoo.com>
2023-03-18 19:46:00 -0700

    added loop for password length and validation, added while(true)loop for choosing character type with validation for at least one being chosen, added message with information on which chartypes were chosen, created var of all available characters and created a for loop for choosing random characters from those available iterated *passwordlength.

--- script.js
+++ script.js
@@ -18,16 +18,82 @@ function writePassword() {
   //click to get a password
   var passwordNeeded = confirm("Would you like a secure password?");
   
+  //loop for length of the password
   //password length
-  var passwordLength = prompt("Enter your password length. It must have at least 8 and no more than 128 charcters");
-  
+  while(true) {
+    var passwordLength = prompt("Enter your password length. It must have at least 8 and no more than 128 charcters");
   //validate password length
   if (passwordLength >= 8 && 128 >= passwordLength && Number.isInteger(parseInt(passwordLength))) {
-  confirm(`Your password length is set to ${passwordLength}.`)
+    confirm(`Your password length is set to ${passwordLength}.`);
+    break;
   } else if(passwordLength < 8 || passwordLength>128 || !Number.isIntegar(passwordLength)) {
-  prompt("Please enter a valid password length. You must use numerical characters and set the length from 8 to 128.");
+  alert("Please enter a valid password length. You must use numerical characters and set the length from 8 to 128.");
   }
   }
+    while(true) {
+    // choose list character options
+    //lowercase
+    var lowercase = confirm("Include lowercase letters in your password?");
+    //uppercase
+    var uppercase = confirm("Include uppercase letters in your password?");
+    //numeric
+    var numeric = confirm("Include numerical characters in your password?");
+    //special
+    var special = confirm("Include special characters in your password?")
+
+    if (lowercase || uppercase || numeric || special) {
+      break;
+    }
+    else if (!lowercase && !uppercase && !numeric && !special) {
+      alert("You must include at least one character type for your password")
+    }
+  } 
+
+  var chartypes = [];
+
+  if (lowercase) {
+    chartypes.push("lowercase letters")
+  }
+  if (uppercase) {
+    chartypes.push("uppercase letters")
+  }if (numeric) {
+    chartypes.push("numerical characters")
+  }if (special) {
+    chartypes.push("special characters")
+  }
+
+  //message showing what character types were chosen
+  alert("You have chosen to include the following in your password: " + chartypes.join(", "))
+  
+
+  //list of all characters included
+  var availchar = "";
+
+  if (lowercase) {
+    availchar += "abcdefghijklmnopqrstuvwxyz"
+  }
+  if (uppercase) {
+    availchar += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  if (numeric) {
+    availchar += "1234567890"
+  }
+  if (special) {
+    availchar += ",.<>?;':/\"[]{}|=-+_)(*&^%$#@!)"
+  }
+
+  //take pick random characters from avail up to password length chosen
+  var password = "";
+  for (var i = 0; i < (passwordLength); i++) {
+    var randomindex = Math.floor(Math.random()*availchar.length);
+    password += availchar.charAt(randomindex);
+  }
+
+  return password
+  
+}
+  
+
   
   
   //click to get a password
