c4dab7cab528fb7a6995f60d9b6f790f953e7911
rikilega <rosedoland33@yahoo.com>
2023-03-18 17:06:15 -0700

    still working on validation of password length.

--- script.js
+++ script.js
@@ -4,32 +4,36 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword() {
-    //click to get a password
-    var passwordNeeded = confirm("Would you like a secure password?");
-    
-    //password length
-    var passwordLength = prompt("Enter your password length. It must have at least 8 and no more than 128 charcters");
-    
-    //validate password length
-    if (passwordLength < 8 || passwordLength>128 || !Number.isIntegar(passwordLength)) {
-      alert("Please enter a valid password length. You must use numerical characters and set the length from 8 to 128.")
-    } else {
-      confirm(lengthConf);
-    } 
-    if (!lengthConf)
-    return passwordLength
-  }
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-}
+  }
+
+  function generatePassword() {
+  //click to get a password
+  var passwordNeeded = confirm("Would you like a secure password?");
+  
+  //password length
+  var passwordLength = prompt("Enter your password length. It must have at least 8 and no more than 128 charcters");
+  
+  //validate password length
+  if (passwordLength < 8 || passwordLength>128 || !Number.isIntegar(passwordLength)) {
+  prompt("Please enter a valid password length. You must use numerical characters and set the length from 8 to 128.");
+  } else if (passwordLength >= 8 && 128 >= passwordLength && Number.isInteger(parseInt(passwordLength))) {
+    confirm("Your password length is set to " + passwordLength + ".")
+  }
+  }
+  
+  
   //click to get a password
   //var passwordNeeded = confirm("Would you like a secure password?");
   //password length
   //var passwordLength = prompt("Enter your password length. It must have at least 8 and no more than 128 charcters");
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
